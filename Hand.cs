7aa2712317c354587cfdfba412e94b29c041423f
Kyle Rohde <krohde13@gmail.com>
2019-07-31 14:58:13 -0500

    init

---
+++ Hand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowMeRock
+{
+    class Hand
+    {
+        //member variable (has)
+        public string choice;
+
+        //constructor (builds)
+        public Hand(string Choice)
+        {
+            choice = Choice;
+        }
+
+        //methods (does)
+        public string AskChoice()
+        {
+            Console.WriteLine("What would you like to throw this round? Rock, paper, scissors, lizard, or spock?");
+            string inputChoice = Console.ReadLine();
+            return inputChoice;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowMeRock
+{
+    class Player
+    {
+        //member variable (has)
+        public int playerNumber;
+        public int winCounter;
+
+        //constructor (builds)
+        public Player(int playerNumber)
+        {
+            this.playerNumber = playerNumber;
+            winCounter = 0;
+        }
+        //methods (does)
+        public string MakeChoice(Hand hand)
+        {
+            string pick = hand.AskChoice();
+            return pick;
+        }
+    }
+}
